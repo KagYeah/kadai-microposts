d4f1a0dd515c59faf7364da12c1eb2e93da3137c
KagYeah <kagyeah.dev@gmail.com>
2022-06-25 04:18:11 +0000

    fix error_messages.blade.php

--- resources/views/commons/error_messages.blade.php
+++ resources/views/commons/error_messages.blade.php
@@ -1,7 +1,7 @@
 @if (count($errors) > 0)
     <ul class="alert alert-danger" role="alert">
         @foreach ($errors->all() as $error)
-            <il class="ml-4">{{ $error }}</il>
+            <li class="ml-4">{{ $error }}</li>
         @endforeach
     </ul>
 @endif
